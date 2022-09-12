a646eda592b6e5647ad77d81c2bb2760b8965b91
Octavio Martins <octaviomartins10@gmail.com>
2022-09-11 21:16:06 -0300

    corrigindo erro projeto

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const TaxaMetabolicaBasal_1 = require("./TaxaMetabolicaBasal");
+const express = require('express');
+const app = express();
+app.use(express.json());
+function validaDados(request, response, next) {
+    const { peso, idade, sexo } = request.query;
+    // Validação
+    if (peso == '' || peso == null) {
+        return response.status(400).json('Peso inválido ou não informado.');
+    }
+    if (idade == '' || idade == null) {
+        return response.status(400).json('Idade inválida ou não informado');
+    }
+    if (sexo != 'M' || sexo != 'F') {
+        return response.status(400).json('Sexo inválido ou não informado.');
+    }
+    return next();
+}
+const taxaMetabolicaBasal = new TaxaMetabolicaBasal_1.TaxaMetabolicaBasal();
+app.get('/calculaTaxaMetabolicaBasal', validaDados, (request, response) => taxaMetabolicaBasal.calculaTaxa(request, response));
+app.listen(3333);
